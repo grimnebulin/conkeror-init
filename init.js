ccbceca75f3456f6dabcab1351d5e08225479676
Sean McAfee <eefacm@gmail.com>
2014-06-03 18:55:32 -0500

    Adds support for a "sites" directory.

--- init.js
+++ init.js
@@ -1,7 +1,8 @@
-(function () {
+(function (do_eval) {
 
     const rcdir   = make_file("/home/mcafee/conkrc");
     const modules = make_file("/home/mcafee/conkrc/modules");
+    const sitedir = make_file("/home/mcafee/conkrc/sites");
 
     load_paths.unshift(make_uri(modules).spec);
 
@@ -12,13 +13,54 @@
         if (file.leafName != "init.js")
             load(file);
 
+    const sites = [ ];
+
+    function load_sites() {
+        Array.prototype.splice.apply(
+            sites, [ 0, sites.length ].concat(
+                [ file for (file of js_iter(sitedir)) ]
+            )
+        );
+    }
+
+    load_sites();
+
+    interactive("reload-sites", "Reload sites directory", load_sites);
+
+    add_hook("buffer_dom_content_loaded_hook", function (buffer) {
+        for (let file of sites_matching(buffer.current_uri.asciiHost))
+            read_file(file, content => do_eval($$(buffer), buffer, content));
+    });
+
     function js_iter(dir) {
+        if (!dir.exists()) return;
         const entries = dir.directoryEntries;
         while (entries.hasMoreElements()) {
-            const entry = entries.getNext().QueryInterface(Ci.nsIFile);
-            if (/\.js$/.test(entry.leafName))
-                yield entry;
+            const file = entries.getNext().QueryInterface(Ci.nsIFile);
+            if (!file.isDirectory() && /\.js$/.test(file.leafName))
+                yield file;
         }
     }
 
-})();
+    function sites_matching(host) {
+        for (let file of sites) {
+            const name = file.leafName.substr(0, file.leafName.length - 3);
+            if (host == name || host.endsWith("." + name))
+                yield file;
+        }
+    }
+
+    Components.utils.import("resource://gre/modules/NetUtil.jsm");
+
+    function read_file(file, callback) {
+        NetUtil.asyncFetch(file, function (stream, status) {
+            if (Components.isSuccessCode(status)) {
+                const content = NetUtil.readInputStreamToString(
+                    stream, stream.available()
+                );
+                callback(content);
+            }
+        });
+    }
+
+})(function ($, buffer, str) { eval(str) });
